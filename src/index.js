914a34e86998cb3ee5421faec512dcc2aeb1201c
cijo.kb <cijo.kb@accenture.com>
2017-11-30 09:44:58 +0530

    Modified readme,gitignore and index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/*
+    It is an app for those who are fed up with W/L status while booking on www.IRCTC.co.in for future dates.
+    This small app will help to know the advance bookable dates(60 days) of the current and next month based on the preferred days.
+    Booking on the dates shown will have chance to get confirmed tickets.
+
+    version 1.0
+    Developed by Cijo K.B
+    email:cijo.kb@gmail.com
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
